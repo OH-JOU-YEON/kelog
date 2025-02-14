425062d92b5753b9517691cb4cf23706eb8063be
hyperclutter <hyperclutter@gmail.com>
2025-02-14 13:56:22 +0900

    처음

---
+++ src/main/java/portfolio/demo/KelogApplication.java
@@ -0,0 +1,13 @@
+package portfolio.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KelogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KelogApplication.class, args);
+	}
+
+}
