45aa4af8c80a0d8d967daca8c5b616034eb8afe0
KYC <maker0472@naver.com>
2025-02-14 17:44:06 +0900

    여행게시판 작성중..

--- kelog/src/main/java/com/spring/domain/TravelPostDTO.java
+++ kelog/src/main/java/com/spring/domain/TravelPostDTO.java
@@ -1,5 +1,16 @@
 package com.spring.domain;
 
-public class TravelPostDTO {
+import java.util.Date;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.Data;
+@Data
+public class TravelPostDTO {
+	private int bno;
+	private String title;
+	private String content;
+	private String writer;
+	@DateTimeFormat(pattern = "yyyy-MM-dd a hh:mm:ss")
+	private Date regDate;
 }
